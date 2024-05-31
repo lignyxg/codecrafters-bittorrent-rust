ceb168cdcfecd207c54559e9f60ca9a2d7e4dac5
lignyxg <lignyxg@qq.com>
2024-05-31 22:03:39 +0800

    parse digits

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,14 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if let Some((len, rest)) = encoded_value.split_once(':') {
+    if let Some(rest) = encoded_value.strip_prefix('i') {
+        if let Some((digits, _)) = rest.split_once('e') {
+            if let Ok(n) = digits.parse::<i64>() {
+                // serde_json::Value::Number(n.into())
+                return n.into();
+            }
+        }
+    } else if let Some((len, rest)) = encoded_value.split_once(':') {
         if let Ok(len) = len.parse::<usize>() {
             return serde_json::Value::String(rest[..len].to_string());
         }
