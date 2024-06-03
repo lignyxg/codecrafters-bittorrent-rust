ba63cbd6d82a1417b54d61340e6bef910bec7501
lignyxg <lignyxg@qq.com>
2024-06-03 15:15:19 +0800

    parse peers

--- src/main.rs
+++ src/main.rs
@@ -1,177 +1,13 @@
 use anyhow::Context;
 use clap::Parser;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use sha1::{Digest, Sha1};
 
-use bittorrent_starter_rust::{Args, Commands, Hashes};
-
-// Available if you need it!
-// use serde_bencode
-
-/// Metainfo files (also known as .torrent files) are bencoded dictionaries with the following keys:
-#[derive(Debug, Clone, Deserialize, Serialize)]
-struct Torrent {
-    /// The URL of the tracker
-    announce: String,
-    /// This maps to a dictionary, with keys described below.
-    info: Info,
-}
-
-#[derive(Debug, Deserialize, Clone, Serialize)]
-struct Info {
-    /// The name key maps to a UTF-8 encoded string which is the suggested name to
-    /// save the file (or directory) as. It is purely advisory.
-    /// In the single file case, the name key is the name of a file,
-    /// in the muliple file case, it's the name of a directory.
-    name: String,
-    /// The number of bytes in each piece the file is split into.
-    /// For the purposes of transfer, files are split into fixed-size pieces which are
-    /// all the same length except for possibly the last one which may be truncated.
-    /// piece length is almost always a power of two,
-    /// most commonly 2^18 = 256 K (BitTorrent prior to version 3.2 uses 2^20 = 1 M as default).
-    #[serde(rename = "piece length")]
-    plength: usize,
-    /// pieces maps to a string whose length is a multiple of 20.
-    /// It is to be subdivided into strings of length 20,
-    /// each of entry of `pieces` is the SHA1 hash of the piece at the corresponding index.
-    pieces: Hashes,
-    /// There is also a key length or a key files, but not both or neither.
-
-    #[serde(flatten)]
-    keys: Keys,
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(untagged)]
-enum Keys {
-    /// If length is present then the download represents a single file,
-    /// In the single file case, length maps to the length of the file in bytes.
-    SingleFile { length: usize },
-
-    /// otherwise it represents a set of files which go in a directory structure.
-    /// the multi-file case is treated as only having a single file by concatenating
-    /// the files in the order they appear in the files list. The files list is the value
-    /// files maps to, and is a list of dictionaries containing the following keys:
-    MultiFile { files: Vec<File> },
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-struct File {
-    /// The length of the file, in bytes.
-    length: usize,
-    /// A list of UTF-8 encoded strings corresponding to subdirectory names,
-    /// the last of which is the actual file name (a zero length list is an error case).
-    path: Vec<String>,
-}
-
-fn decode_bencoded(encoded_value: &str) -> (serde_json::Value, &str) {
-    match encoded_value.chars().next() {
-        Some('i') => {
-            eprintln!("i: {}", encoded_value);
-            if let Some((pre, rest)) = encoded_value.split_once('e') {
-                let mut pre = pre.to_string();
-                pre.push('e');
-                let v = serde_bencode::from_str::<serde_json::Number>(&pre).unwrap();
-                return (v.into(), rest);
-            }
-        }
-        Some('l') => {
-            let mut rest = encoded_value.split_at(1).1;
-            let mut values = Vec::new();
-            while !rest.is_empty() && !rest.starts_with('e') {
-                let (v, remainder) = decode_bencoded(rest);
-                values.push(v);
-                rest = remainder;
-                eprintln!("rest: {rest}");
-            }
-            return (values.into(), &rest[1..]);
-        }
-        Some('d') => {
-            let mut dict = serde_json::Map::new();
-            let mut rest = encoded_value.split_at(1).1;
-            while !rest.is_empty() && !rest.starts_with('e') {
-                let (k, remainder) = decode_bencoded(rest);
-                let k = match k {
-                    Value::String(k) => k,
-                    k => panic!("dict keys should be string, not {k:?}"),
-                };
-                let (v, remainder) = decode_bencoded(remainder);
-                dict.insert(k, v);
-                rest = remainder;
-            }
-            return (dict.into(), &rest[1..]);
-        }
-        Some('0'..='9') => {
-            let n = encoded_value.split_at(1).0.parse::<usize>().unwrap();
-            let (pre, rest) = encoded_value.split_at(n + 2);
-            let v = serde_bencode::from_str::<String>(pre).unwrap();
-            return (v.into(), rest);
-        }
-        _ => {}
-    }
-    panic!("Unhandled encoded value: {}", encoded_value);
-}
-
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
-    // If encoded_value starts with a digit, it's a number
-    match encoded_value.chars().next() {
-        Some('i') => {
-            if let Some((n, rest)) =
-                encoded_value
-                    .split_at(1)
-                    .1
-                    .split_once('e')
-                    .and_then(|(digits, rest)| {
-                        let n = digits.parse::<i64>().ok()?;
-                        Some((n, rest))
-                    })
-            {
-                return (n.into(), rest);
-            }
-        }
-        Some('l') => {
-            let mut values = Vec::new();
-            let mut rest = encoded_value.split_at(1).1;
-            while !rest.is_empty() && !rest.starts_with('e') {
-                let (v, remainder) = decode_bencoded_value(rest);
-                rest = remainder;
-                values.push(v);
-            }
-
-            return (values.into(), &rest[1..]);
-        }
-        Some('d') => {
-            let mut dict = serde_json::Map::new();
-            let mut rest = encoded_value.split_at(1).1;
-            while !rest.is_empty() && !rest.starts_with('e') {
-                let (k, remainder) = decode_bencoded_value(rest);
-                let k = match k {
-                    Value::String(k) => k,
-                    k => panic!("dict keys should be string, not {k:?}"),
-                };
-                let (v, remainder) = decode_bencoded_value(remainder);
-                rest = remainder;
-                dict.insert(k, v);
-            }
-
-            return (dict.into(), &rest[1..]);
-        }
-        Some('0'..='9') => {
-            if let Some((len, rest)) = encoded_value.split_once(':') {
-                if let Ok(len) = len.parse::<usize>() {
-                    return (rest[..len].to_string().into(), &rest[len..]);
-                }
-            }
-        }
-        _ => {}
-    }
-    panic!("Unhandled encoded value: {}", encoded_value);
-}
+use bittorrent_starter_rust::{
+    Args, Commands, decode_bencoded, Torrent, TrackerRequest, TrackerResponse, urlencode,
+};
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     match args.commands {
         Commands::Decode { value } => {
@@ -183,16 +19,11 @@ fn main() -> anyhow::Result<()> {
             let t: Torrent = serde_bencode::from_bytes(&f).context("parse torrent file")?;
             // eprintln!("{t:?}");
             println!("Tracker URL: {}", t.announce);
-            if let Keys::SingleFile { length } = t.info.keys {
-                println!("Length: {}", length);
-            } else {
-                todo!()
-            }
-            let info_encoded =
-                serde_bencode::to_bytes(&t.info).context("re-encode info section")?;
-            let mut hasher = Sha1::new();
-            hasher.update(&info_encoded);
-            let info_hash = hasher.finalize();
+
+            let length = t.length();
+            println!("Length: {}", length);
+
+            let info_hash = t.info_hash();
             println!("Info Hash: {}", hex::encode(info_hash));
             println!("Piece Length: {}", t.info.plength);
             println!("Piece Hashes:");
@@ -200,6 +31,44 @@ fn main() -> anyhow::Result<()> {
                 println!("{}", hex::encode(hash));
             }
         }
+        Commands::Peers { torrent } => {
+            let f = std::fs::read(torrent).context("read torrent file")?;
+            let t: Torrent = serde_bencode::from_bytes(&f).context("parse torrent file")?;
+
+            let length = t.length();
+            let info_hash = t.info_hash();
+            let request = TrackerRequest {
+                peer_id: "00112233445566778899".to_string(),
+                port: 6881,
+                uploaded: 0,
+                downloaded: 0,
+                left: length,
+                compact: 1,
+            };
+
+            let url_params =
+                serde_urlencoded::to_string(&request).context("url-encode tracker parameters")?;
+
+            let tracker_url = format!(
+                "{}?{}&info_hash={}",
+                t.announce,
+                url_params,
+                &urlencode(&info_hash)
+            );
+
+            let response = reqwest::get(tracker_url)
+                .await
+                .context("query tracker")?
+                .bytes()
+                .await
+                .context("fetch tracker response")?;
+            let response: TrackerResponse =
+                serde_bencode::from_bytes(&response).context("parse tracker response")?;
+            for peer in response.peers.0 {
+                println!("{}:{}", peer.ip(), peer.port());
+            }
+        }
     }
+
     Ok(())
 }
